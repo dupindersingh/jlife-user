739f669522ac306c69352f33a272e0a61ab7de0d
devserver <gittest@iapptechnologies.com>
2019-07-17 13:13:15 +0530

    fixed the login issue

--- src/actions/logIn.js
+++ src/actions/logIn.js
@@ -17,45 +17,106 @@ const logInEmail = () => {
         .get()
         .then(snapshot => {
             if (snapshot.empty) {
-                console.log('No matching documents.');
-                return;
-            } else {
-                snapshot.forEach(doc => {
-                    const document = Firebase.firestore().collection("users").doc(doc.id).get();
-                    document.then((doc) => {
-                        if (doc) {
-                            if (doc.data().email === email) {
-                                isEmailExist = true;
-                                return Firebase.auth().signInWithEmailAndPassword(email, password)
-                                    .then(async function (user) {
-                                        if (!user.displayName) {
-                                            const userDoc = await Firebase.firestore().collection("users").doc(user.email).get();
-                                            user.updateProfile({
-                                                displayName: (userDoc.data().firstName + " " + userDoc.data().lastName)
-                                            })
-                                        }
-
-                                    })
-                                    .then(function () {
-                                        window.location.replace('./menu');
-                                    })
-                                    .catch(function (error) {
-                                        var errorCode = error.code;
-                                        var errorMessage = error.message;
-                                        console.log('Error code: ' + errorCode);
-                                        console.log('Error message: ' + errorMessage);
-                                        if (!!loginButton && !!loginError) {
-                                            loginButton.style.display = 'block';
-                                            loginError.innerText = errorMessage;
-                                            loginError.style.display = 'block';
-                                        }
-                                    });
+                window.setTimeout(() => {
+                    Firebase.firestore().collection("users").where("isAdmin", "==", false)
+                        .get()
+                        .then(snapshot => {
+                            console.log(snapshot, "snaphot.....");
+                            if (snapshot.empty) {
+                                var errorMessage = "No users or managers found!!";
+                                console.log('Error message: ' + errorMessage);
+                                if (!!loginButton && !!loginError) {
+                                    loginButton.style.display = 'block';
+                                    loginError.innerText = errorMessage;
+                                    loginError.style.display = 'block';
+                                }
+                                return;
                             }
-                        }
-                    })
-                })
+                            snapshot.forEach(doc => {
+                                const document = Firebase.firestore().collection("users").doc(doc.id).get();
+                                document.then((doc) => {
+                                    if (doc) {
+                                        console.log(doc.data().email, "searched email", email, "entered email");
+                                        if (doc.data().email === email) {
+                                            isEmailExist = true;
+                                            return Firebase.auth().signInWithEmailAndPassword(email, password)
+                                                .then(async function (user) {
+                                                    if (!user.displayName) {
+                                                        const userDoc = await Firebase.firestore().collection("users").doc(user.email).get();
+                                                        user.updateProfile({
+                                                            displayName: (userDoc.data().firstName + " " + userDoc.data().lastName)
+                                                        })
+                                                    }
 
+                                                })
+                                                .then(function () {
+                                                    window.location.replace('./menu');
+                                                })
+                                                .catch(function (error) {
+                                                    var errorCode = error.code;
+                                                    var errorMessage = error.message;
+                                                    console.log('Error code: ' + errorCode);
+                                                    console.log('Error message: ' + errorMessage);
+                                                    if (!!loginButton && !!loginError) {
+                                                        loginButton.style.display = 'block';
+                                                        loginError.innerText = errorMessage;
+                                                        loginError.style.display = 'block';
+                                                    }
+                                                });
+                                        }
+                                    }
+                                })
+                            })
+                        })
+                        .then(() => {
+                            window.setTimeout(() => {
+                                if (!isEmailExist) {
+                                    var errorMessage = "Email doesn't exist";
+                                    if (!!loginButton && !!loginError) {
+                                        loginButton.style.display = 'block';
+                                        loginError.innerText = errorMessage;
+                                        loginError.style.display = 'block';
+                                    }
+                                }
+                            }, 4000)
+                        })
+                }, 5000)
             }
+            snapshot.forEach(doc => {
+                const document = Firebase.firestore().collection("users").doc(doc.id).get();
+                document.then((doc) => {
+                    if (doc) {
+                        console.log(doc.data().email, "searched email", email, "entered email");
+                        if (doc.data().email === email) {
+                            isEmailExist = true;
+                            return Firebase.auth().signInWithEmailAndPassword(email, password)
+                                .then(async function (user) {
+                                    if (!user.displayName) {
+                                        const userDoc = await Firebase.firestore().collection("users").doc(user.email).get();
+                                        user.updateProfile({
+                                            displayName: (userDoc.data().firstName + " " + userDoc.data().lastName)
+                                        })
+                                    }
+
+                                })
+                                .then(function () {
+                                    window.location.replace('./menu');
+                                })
+                                .catch(function (error) {
+                                    var errorCode = error.code;
+                                    var errorMessage = error.message;
+                                    console.log('Error code: ' + errorCode);
+                                    console.log('Error message: ' + errorMessage);
+                                    if (!!loginButton && !!loginError) {
+                                        loginButton.style.display = 'block';
+                                        loginError.innerText = errorMessage;
+                                        loginError.style.display = 'block';
+                                    }
+                                });
+                        }
+                    }
+                })
+            })
         })
         .then(() => {
             window.setTimeout(() => {
@@ -67,7 +128,7 @@ const logInEmail = () => {
                         loginError.style.display = 'block';
                     }
                 }
-            }, 4000)
+            }, 8000)
         })
 };
 
